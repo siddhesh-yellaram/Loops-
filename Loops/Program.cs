9301f8bfa2d8755a2a49326407b7d3a6e289e7fc
Siddhesh Yellaram <58585975+siddhesh-yellaram@users.noreply.github.com>
2021-08-07 01:23:10 +0530

    Add files via upload

---
+++ Loops/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string secretWord = "kite";
+            //string guess = "";
+
+            //while (guess != secretWord)
+            //{
+            //    Console.Write("Enter your guess: ");
+            //    guess = Console.ReadLine();
+            //}
+
+            //Console.WriteLine("You Win!!!");
+
+            //for(int i = 1; i<=5; i++)
+            //{
+            //    Console.WriteLine("Square of  " + i + " is "+(i * i));
+            //}
+
+            //foreach(int n in nums)
+            //{
+            //    Console.WriteLine("Cube of " + n + " is " + (n * n * n));
+            //}
+
+            //for(int i = 0; i < nums.Length; i++)
+            //{
+            //    Console.WriteLine("Cube of " + nums[i] + " is " + ( nums[i]* nums[i] * nums[i]));
+            //}
+
+            char[] vowels = { 'a', 'i', 'e', 'o', 'u' };
+            int i = 0;
+            
+            while(i < vowels.Length)
+            {
+                Console.WriteLine("Value at index " + i + " is " + vowels[i]);
+                i++;
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
